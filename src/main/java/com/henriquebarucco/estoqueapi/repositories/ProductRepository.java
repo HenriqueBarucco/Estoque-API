305cb2dd8783f6310ee07f5089b940ce0bc47773
Henrique Barucco <henrique.sanches@criar.com>
2023-04-19 11:44:11 -0300

    fix: removendo import

--- src/main/java/com/henriquebarucco/estoqueapi/repositories/ProductRepository.java
+++ src/main/java/com/henriquebarucco/estoqueapi/repositories/ProductRepository.java
@@ -5,7 +5,6 @@ import com.henriquebarucco.estoqueapi.entities.Product;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
